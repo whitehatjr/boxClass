2031285e8cef30837cf6a811455a230f40dba130
SuhaniRana <70816383+SuhaniRana@users.noreply.github.com>
2020-09-19 14:09:49 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,38 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
+var car, wall
+var speed, weight
 
-var engine, world;
-var box1;
+function setup() {
+  createCanvas(1600,400);
+  //createSprite(400, 200, 50, 50);
 
-function setup(){
-    var canvas = createCanvas(400,400);
-    engine = Engine.create();
-    world = engine.world;
+  speed=random(55,90);
+  weight=random(400,1500);
+  car=createSprite(50,200,50,50);
+  wall=createSprite(1500,200,60,height/2);
+  wall.shapeColor=color(80,80,80);
+  car.velocityX=speed;
 
-    box1 = new Box(200,300,50,50);
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-    box1.display();
-   
+function draw() {
+  background(0);  
+  
+
+  if(wall.x-car.x <(car.width+wall.width)/2) {
+
+car.velocityX=0;
+var deformation = 0.5 * weight * speed* speed/22509;
+
+if(deformation>180){
+
+  car.shapeColor=color(255,0,0);
+}
+
+if(deformation<100){
+  car.shapeColor=color(0,255,0);
+}
+
+  }
+ console.log(deformation);
+  drawSprites();
 }
